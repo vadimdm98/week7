a523590441dced79f741642049f81284404caa3f
vadimdm98 <101522820+vadimdm98@users.noreply.github.com>
2022-05-20 13:30:53 +0300

    Add files via upload

---
+++ Discount/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Discount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the purchase amount of your check in our store?");
+            Console.WriteLine("Enter amount (integer) : ");
+            int totalCost = Convert.ToInt32(Console.ReadLine());
+
+            if (totalCost < 10)
+            {
+                Console.WriteLine("Unfortunately next time there will be no discount.");
+            }
+            else if (totalCost >= 10 && totalCost <= 20)
+            {
+                Console.WriteLine("Your discount on your next purchase in our store will be 1 euro");
+            }
+            else if (totalCost > 20 && totalCost <= 50)
+            {
+                Console.WriteLine("Your discount on your next purchase in our store will be 5 euro");
+            }
+            else if (totalCost > 50 && totalCost <= 100)
+            {
+                Console.WriteLine("Your discount on your next purchase in our store will be 10 euro");
+            }
+            else
+            {
+                Console.WriteLine("Your discount on your next purchase in our store will be 15 euro");
+            }
+
+            Console.WriteLine("Have a nice day!");
+        }
+    }
+}
---
+++ RockScissorsPaper/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace RockScissorsPaper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - камень
+            //2 - ножницы
+            //3 - бумага
+            //камень бьет ножницы, ножницы режут бумагу, бумага оборачивает камень
+            //игра должна продолжаться до того момента, пока один из игроков не наберет 3 очка
+
+            int userScore = 0;
+            int cpuScore = 0;
+
+            Console.WriteLine("Rock - Paper - Scissors Game");
+            Console.WriteLine("plays up to three points");
+            Console.WriteLine("Variant choice:");
+
+            while (userScore < 3 && cpuScore < 3)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Rock - Paper - Scissors");
+                string userChoice = Console.ReadLine().ToLower();
+
+                Console.WriteLine($"USER choice {userChoice}"); Random rnd = new Random();
+                int random = rnd.Next(1, 4);
+                string cpuChoice = "";
+
+                switch (random)
+                {
+                    case 1:
+                        cpuChoice = "paper";
+                        break;
+                    case 2:
+                        cpuChoice = "rock";
+                        break;
+                    case 3:
+                        cpuChoice = "scissors";
+                        break;
+                }
+
+                Console.WriteLine($"CPU choice {cpuChoice}");
+
+                if ((cpuChoice == "paper" && userChoice == "rock") || (cpuChoice == "scissors" && userChoice == "paper") || (cpuChoice == "rock" && userChoice == "scissors"))
+                {
+                    cpuScore++;
+                    Console.WriteLine("USER lose. CPU wins!");
+                }
+                else if ((userChoice == "rock" && cpuChoice == "scissors") || (userChoice == "paper" && cpuChoice == "rock") || (userChoice == "scissors" && cpuChoice == "paper"))
+                {
+                    userScore++;
+                    Console.WriteLine("USER win! CPU lose.");
+                }
+                else if (userChoice == cpuChoice)
+                {
+                    Console.WriteLine("This game was a draw.");
+                }
+                else
+                {
+                    cpuScore++;
+                    Console.WriteLine("Invalid input from user, CPU wins.");
+                }
+
+                Console.WriteLine($"USER score: {userScore} - CPU score: {cpuScore}");
+
+            }
+
+            if (userScore == 3)
+            {
+                userScore++;
+                Console.WriteLine("User Wins");
+
+            }
+            else
+            {
+                cpuScore++;
+                Console.WriteLine("Cpu Wins");
+
+            }
+
+            Console.WriteLine("Have a nice day!");
+        }
+    }
+}
